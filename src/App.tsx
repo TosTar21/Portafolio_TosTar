c2eacbfaf94933a2fa12dee0879543cf980b51f7
TosTar21 <oscar.zuniga.sanchez@est.una.ac.cr>
2025-02-26 10:31:40 -0600

    Projet's Struicture

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,12 @@
-import './App.css'
+import '../src/styles/index.css'
 
 function App() {
   
   return (
     <>
+    <h1 className='text-4xl text-center text-blue-500'>
+      Hello
+    </h1>
     </>
   )
 }
