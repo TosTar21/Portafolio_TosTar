29553bfdb9913e9d7514cb1eb301664e306ff0cb
Tostar21 <oscar.zuniga.sanchez@est.una.ac.cr>
2025-07-11 09:02:01 -0600

    refactor: i refactored positio of the hero and also i added the shadiow in the navlinks

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
-import MainLayout from "./components/Layout/MainLayout";
 import { useTheme } from "./hooks/useTheme";
 import Work from "./pages/Work";
 import { Route, BrowserRouter as Router, Routes } from "react-router";
 import "./styles/index.css";
 import About from "./pages/About";
 import CVPage from "./pages/CVPage";
+import Navbar from "./components/microcomponents/Navbar";
+import Footer from "./components/microcomponents/Footer";
+import ThemeIcon from "./components/microcomponents/ThemeIcon";
 
 const App = () => {
   const { isDarkMode } = useTheme();
@@ -12,13 +14,18 @@ const App = () => {
   return (
     <div className={`${isDarkMode ? "bg-neutral-800 text-white" : "bg-neutral-100 text-black"} min-h-screen transition-all duration-500`}>
       <Router>
-        <MainLayout>
-          <Routes>
-            <Route path="/" element={<Work />} />
-            <Route path="/about" element={<About/>} />
-            <Route path="/resume" element={<CVPage />} />
-          </Routes>
-        </MainLayout>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={
+            <>
+              <ThemeIcon />
+              <Work />
+            </>
+          } />
+          <Route path="/about" element={<About />} />
+          <Route path="/resume" element={<CVPage />} />
+        </Routes>
+        <Footer />
       </Router>
     </div>
   );
